e6a86fadbf9fe394f52d3f4a6910b46f81dd3a0e
GOLVEN666 <achraf.xchahid@gmail.com>
2024-07-19 16:29:04 +0100

    this is my boy

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+
+
+export default defineConfig({
+  devOptions: {
+    hostname: '8.4.8.4', // Use '0.0.0.0' to listen on all network interfaces
+  },
+});
